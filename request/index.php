7250e5e87346f10522ed24701f0c2b28750a07cf
Angelo Aracri <angeloaracri@yahoo.de>
2017-03-13 17:12:04 +0100

    Adjusted request interface to new Request class system

--- request/index.php
+++ request/index.php
@@ -1,15 +1,36 @@
 <?php
 
+// Load app
 require_once("../php/app.php");
 $app = new App();
 
+// Check authentification
 if ($app->auth() < 1) {
   die ('{"success":false, "error":"No sufficient permissions"}');
 }
 
+// Load super Request class and its extensions
+include("../php/requests/request.php");
 
+// Get Requests name
+if (!isset($_POST['requestName'])) {
+  $app->err("Did not specifiy a requestName");
+}
+$requestName = $_POST['requestName'];
 
+// Get corresponding Request
+$requestHandler = Request::getRequestByName($requestName);
+if ($requestHandler == NULL) {
+  $app->err("Could not find request with name \"" . $requestName . "\"");
+}
 
+// Call getResult with optional RequestData
+$jsonResult = $action->getResult($app, $_POST['requestData'] || NULL);
+if ($jsonResult == NULL) {
+  $app->err("Could not execute \"" . $requestName . "\"; result was NULL");
+}
 
+// Return result
+die ('{"success":true, "result":' . json_encode($jsonResult) . '}');
 
 ?>
