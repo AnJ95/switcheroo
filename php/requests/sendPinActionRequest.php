b82c4e75fe0fddc317609ac04dec17eb48e26d31
Angelo Aracri <angeloaracri@yahoo.de>
2017-03-13 20:06:38 +0100

    Recreated sendPinActionRequest

--- php/requests/sendPinActionRequest.php
+++ php/requests/sendPinActionRequest.php
@@ -2,6 +2,29 @@
 
 class SendPinActionRequest extends Request {
     public function getResult($app, $requestData) {
+      if (!isset($requestData['type'])) {
+        $app->err("Could not perform SendPinActionRequest because action type was not specified");
+      }
+
+      $pin = intval($requestData['pin']);
+
+      switch ($requestData['type']) {
+        case "toggle":
+          pinWrite($pin, ($requestData['value'] == "true" ? 0 : 1));
+          break;
+        case "pulse":
+          pinWrite($pin, 0);
+          usleep(10000);
+          pinWrite($pin, 1);
+          usleep(10000);
+          pinWrite($pin, 0);
+          break;
+        default:
+          $app->err("Could not perform SendPinActionRequest because action type \"" . $requestData['type'] . "\" is unknown");
+          return NULL;
+          break;
+      }
+
       return [];
     }
 }
