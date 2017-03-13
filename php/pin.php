91113d60fd731b2496539311d496608f04d14c94
Angelo Aracri <angeloaracri@yahoo.de>
2017-03-13 19:48:01 +0100

    Recreated getPinActionsRequest

--- php/pin.php
+++ php/pin.php
@@ -1,7 +1,13 @@
 <?php
 
 function pinWrite($pin, $write) {
+  // TODO CHECK INPUT!
   shell_exec("gpio write " . $pin . " " . $write);
+}
+
+function pinRead($pin) {
+  // TODO CHECK INPUT!
+  return trim(shell_exec("gpio read " . $pin)) == "1";
 }
 
 ?>
--- php/requests/getPinActionsRequest.php
+++ php/requests/getPinActionsRequest.php
@@ -1,8 +1,19 @@
 <?php
 
+include("../php/pin.php");
+
 class GetPinActionsRequest extends Request {
     public function getResult($app, $requestData) {
-      return [];
+      $buttons = $app->config("pinActions");
+
+      // set default values
+      foreach($buttons as $i => $button) {
+        if ($button->action->type == "toggle") {
+          $button->action->default = (pinRead($button->action->pin) ? true : false);
+        }
+      }
+
+      return $buttons;
     }
 }
 
