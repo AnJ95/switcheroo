ec5ed07cfcfa94f8f1bc9a1eeee7ad8b1588434d
Angelo Aracri <angeloaracri@yahoo.de>
2017-03-17 15:06:38 +0100

    Changed the way, pins are formatted in getPinsRequest server-sided

--- php/requests/getPinsRequest.php
+++ php/requests/getPinsRequest.php
@@ -4,6 +4,7 @@ error_reporting(0);
 class GetPinsRequest extends Request {
     public function getResult($app, $requestData) {
       $result = shell_exec("gpio readall");
+      $result = "+-----+-----+---------+------+---+---Pi 3---+---+------+---------+-----+-----+\n| BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |\n+-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+\n|     |     |    3.3v |      |   |  1 || 2  |   |      | 5v      |     |     |\n|   2 |   8 |   SDA.1 |   IN | 1 |  3 || 4  |   |      | 5v      |     |     |\n|   3 |   9 |   SCL.1 |   IN | 1 |  5 || 6  |   |      | 0v      |     |     |\n|   4 |   7 | GPIO. 7 |   IN | 1 |  7 || 8  | 0 | IN   | TxD     | 15  | 14  |\n|     |     |      0v |      |   |  9 || 10 | 1 | IN   | RxD     | 16  | 15  |\n|  17 |   0 | GPIO. 0 |   IN | 0 | 11 || 12 | 0 | IN   | GPIO. 1 | 1   | 18  |\n|  27 |   2 | GPIO. 2 |   IN | 0 | 13 || 14 |   |      | 0v      |     |     |\n|  22 |   3 | GPIO. 3 |   IN | 0 | 15 || 16 | 0 | IN   | GPIO. 4 | 4   | 23  |\n|     |     |    3.3v |      |   | 17 || 18 | 0 | IN   | GPIO. 5 | 5   | 24  |\n|  10 |  12 |    MOSI |   IN | 0 | 19 || 20 |   |      | 0v      |     |     |\n|   9 |  13 |    MISO |   IN | 0 | 21 || 22 | 0 | IN   | GPIO. 6 | 6   | 25  |\n|  11 |  14 |    SCLK |   IN | 0 | 23 || 24 | 1 | IN   | CE0     | 10  | 8   |\n|     |     |      0v |      |   | 25 || 26 | 1 | IN   | CE1     | 11  | 7   |\n|   0 |  30 |   SDA.0 |   IN | 1 | 27 || 28 | 1 | IN   | SCL.0   | 31  | 1   |\n|   5 |  21 | GPIO.21 |   IN | 1 | 29 || 30 |   |      | 0v      |     |     |\n|   6 |  22 | GPIO.22 |   IN | 1 | 31 || 32 | 0 | IN   | GPIO.26 | 26  | 12  |\n|  13 |  23 | GPIO.23 |   IN | 0 | 33 || 34 |   |      | 0v      |     |     |\n|  19 |  24 | GPIO.24 |   IN | 0 | 35 || 36 | 0 | IN   | GPIO.27 | 27  | 16  |\n|  26 |  25 | GPIO.25 |   IN | 0 | 37 || 38 | 0 | IN   | GPIO.28 | 28  | 20  |\n|     |     |      0v |      |   | 39 || 40 | 0 | OUT  | GPIO.29 | 29  | 21  |\n+-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+\n| BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |\n+-----+-----+---------+------+---+---Pi 3---+---+------+---------+-----+-----+";
       // Returns string that looks sth like this:
       // +-----+-----+---------+------+---+---Pi 3---+---+------+---------+-----+-----+
       // | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
@@ -33,9 +34,8 @@ class GetPinsRequest extends Request {
       // +-----+-----+---------+------+---+---Pi 3---+---+------+---------+-----+-----+
 
 
-      // This is the data object that will hold our data, separated in two columns
-      // (Thats how you seperate model and view!)
-      $d = array(array(), array());
+      // This is the data object that will hold the data
+      $d = array();
 
       // iterate each line, get two entries each (because there are two columns)
       $lines = explode("\n", $result);
@@ -44,22 +44,23 @@ class GetPinsRequest extends Request {
 
         // parse left column
         $l = array();
-        $l["id"] = trim(substr($line, 10, 2));
+        $l["wPi"] = trim(substr($line, 10, 2));
         $l["name"] = trim(substr($line, 15, 7));
         $l["dir"] = trim(substr($line, 26, 3));
-        $l["val"] = trim(substr($line, 32, 1));
-        $d[0][] = $l;
+        $l["val"] = trim(substr($line, 31, 1));
+        $id = trim(substr($line, 35, 2));
+        $d[$id] = $l;
 
         // parse right column
         $l = array();
-        $l["id"] = trim(substr($line, 68, 2));
+        $l["wPi"] = trim(substr($line, 68, 2));
         $l["name"] = trim(substr($line, 58, 7));
         $l["dir"] = trim(substr($line, 51, 3));
         $l["val"] = trim(substr($line, 47, 1));
-        $d[1][] = $l;
+        $id = trim(substr($line, 41, 2));
+        $d[$id] = $l;
 
       }
-
       return $d;
     }
 }
