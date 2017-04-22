7f3958020c9fd41249fa1cfc455bd0228dab82a2
Angelo Aracri <angeloaracri@yahoo.de>
2017-04-22 19:48:44 +0200

    Added backend for pwm stuff

--- php/pin.php
+++ php/pin.php
@@ -5,9 +5,15 @@ function pinWrite($pin, $write) {
   shell_exec("gpio write " . $pin . " " . $write);
 }
 
+function pinPWM($pin, $pwm) {
+  // TODO CHECK INPUT!
+  shell_exec("gpio pwm " . $pin . " " . round($pwm * 1000));
+}
+
 function pinRead($pin) {
   // TODO CHECK INPUT!
   return trim(shell_exec("gpio read " . $pin)) == "1";
 }
+
 
 ?>
--- php/requests/sendPinActionRequest.php
+++ php/requests/sendPinActionRequest.php
@@ -7,13 +7,21 @@ class SendPinActionRequest extends Request {
         $app->err("Could not perform SendPinActionRequest because action type was not specified");
       }
 
-      $pin = intval($requestData['wPin']);
+
 
       switch ($requestData['type']) {
         case "toggle":
+          $pin = intval($requestData['wPin']);
           pinWrite($pin, ($requestData['value'] == "true" ? 1 : 0));
           break;
+        case "rgbled":
+          var_dump($requestData['values']);
+          pinPWM(intval($requestData['pins']['red']), floatval($requestData['values']['red']));
+          pinPWM(intval($requestData['pins']['green']), floatval($requestData['values']['red']));
+          pinPWM(intval($requestData['pins']['blue']), floatval($requestData['values']['blue']));
+          break;
         case "pulse":
+          $pin = intval($requestData['wPin']);
           pinWrite($pin, 0);
           usleep(100000);
           pinWrite($pin, 1);
