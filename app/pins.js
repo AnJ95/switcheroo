b0fa40b0fd492e614e5d79277a54d06b41533fd7
Angelo Aracri <angeloaracri@yahoo.de>
2017-05-05 16:05:27 +0200

    Added some fun rgb programs in app/pins.js

--- app/pins.js
+++ app/pins.js
@@ -28,4 +28,54 @@ Pins.prototype.toJSON = function() {
   return result;
 }
 
+
+
+
+// var x = 0;
+//
+// setInterval(function () {
+//
+//   r = (1 + Math.sin(x * 0.1 * Math.PI * 2)) / 2;
+//   g = (1 + Math.sin(x * 0.1 * Math.PI * 2 +(Math.PI * 2 *0.333))) / 2;
+//   b = (1 + Math.sin(x * 0.1 * Math.PI * 2 +(Math.PI * 2 *0.666))) / 2;
+//
+//   innerPins[16].writePwm(r);
+//   innerPins[20].writePwm(g);
+//   innerPins[21].writePwm(b);
+//
+//   console.log(r + " " + g + " " + b);
+//
+//
+//   x += 0.01;
+// }, 10);
+
+
+
+// PHI-MULTIPLIER
+// var r = Math.random();
+// var g = Math.random();
+// var b = Math.random();
+//
+// var multiplier = 1.61803398875;
+// setInterval(function () {
+//
+//   r = (r * multiplier) % 1;
+//   g = (g * multiplier) % 1;
+//   b = (b * multiplier) % 1;
+//
+//   innerPins[16].writePwm(r);
+//   innerPins[20].writePwm(g);
+//   innerPins[21].writePwm(b);
+//
+//   console.log(r + " " + g + " " + b);
+// }, 200);
+
+
+// FULLY RANDOM
+// window.setInterval(function () {
+//   innerPins[16].writePwm(Math.random());
+//   innerPins[20].writePwm(Math.random());
+//   innerPins[21].writePwm(Math.random());
+// }, 100);
+
 module.exports = Pins;
